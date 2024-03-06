296e8f181121867967b5ac65d9a84aed8d3183ee
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-06 14:47:18 +0300

    front

--- simpleatom/simpleatom/settings.py
+++ simpleatom/simpleatom/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '.well-known/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '.well-known/'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
