a9cea36d5b93215ece5a3824d0d1244c8907ad53
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-07 19:50:59 +0300

    Cert

--- simpleatom/endpoints.py
+++ simpleatom/endpoints.py
@@ -17,10 +17,10 @@ class Endpoints:
         async def get_by_id():
             return {'error': 'hello'}
 
-        @app.route("/.well-known/acme-challenge/<path:filename>", methods=['GET'])
-        async def sdsa(filename):
-            path = "/root/simpleatom_form/static"
-            return send_from_directory(path, filename)
+        # @app.route("/.well-known/acme-challenge/<path:filename>", methods=['GET'])
+        # async def sdsa(filename):
+        #     path = "/root/simpleatom_form/static"
+        #     return send_from_directory(path, filename)
 
     app.run(host="127.0.0.1", port=int(os.environ.get('PORT', 8000)))
 
