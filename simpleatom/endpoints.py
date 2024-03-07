86b0ea3fb91302149d9966d8a7438989c5a44a67
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-07 19:48:01 +0300

    Cert

--- simpleatom/endpoints.py
+++ simpleatom/endpoints.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -16,6 +16,11 @@ class Endpoints:
         @app.route("/", methods=['GET'])
         async def get_by_id():
             return {'error': 'hello'}
+
+        @app.route("/.well-known/acme-challenge/<path:filename>", methods=['GET'])
+        async def sdsa(filename):
+            path = "/root/simpleatom_form/static"
+            return send_from_directory(path, filename)
 
     app.run(host="127.0.0.1", port=int(os.environ.get('PORT', 8000)))
 
