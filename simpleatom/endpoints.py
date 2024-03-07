7d692a93c04b16fc74c9c315b1b8e65d4f65bc05
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-03-07 20:06:15 +0300

    Cert

--- simpleatom/endpoints.py
+++ simpleatom/endpoints.py
@@ -17,10 +17,21 @@ class Endpoints:
         async def get_by_id():
             return {'error': 'hello'}
 
-        @app.route("/.well-known/acme-challenge/<path:filename>", methods=['GET'])
-        async def sdsa(filename):
-            path = "/root/simpleatom_form/static"
-            return send_from_directory(path, filename)
+        # @app.route("/.well-known/acme-challenge/<path:filename>", methods=['GET'])
+        # async def sdsa(filename):
+        #     path = "/root/simpleatom_form/static"
+        #     return send_from_directory(path, filename)
+
+        @app.route('/<path:path>')
+        def serve_challenge(path):
+            if path.startswith('.well-known/acme-challenge/'):
+                filename = path.split('/')[-1]
+                if filename == '9mOnzU7vV1RSQkcfsnY2RTkDuBtvYACbmJbsxW5I3nk':
+                    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), '9mOnzU7vV1RSQkcfsnY2RTkDuBtvYACbmJbsxW5I3nk.txt')
+                if filename == '3zBWhkXcCIEzwBXkMtMtsChZONKlfrmB7Z29bMSXqPk':
+                    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), '3zBWhkXcCIEzwBXkMtMtsChZONKlfrmB7Z29bMSXqPk.txt')
+
+            return "Not found", 404
 
     app.run(host="127.0.0.1", port=int(os.environ.get('PORT', 8000)))
 
